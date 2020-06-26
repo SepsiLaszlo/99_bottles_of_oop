0d1e929d4b6ef1d8f9847ad50539b364b33f3b2c
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-06-26 13:32:18 +0200

    Extract Bottle Number.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -60,4 +60,44 @@ class BottleNumber
   def initialize(number)
     @number = number
   end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def action(number = 0)
+    if number == 0
+    "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number ==1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
 end
