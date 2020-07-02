9b8405b14be870f3bbcc5d91962a3494132cc935
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-07-02 14:22:31 +0200

    Make Successor Lisko friendly

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -19,8 +19,8 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = bottle_number.successor
 
     "#{bottle_number}".capitalize + " of beer on the wall, " +
     "#{bottle_number} of beer.\n" +
@@ -35,12 +35,23 @@ class BottleNumber
     @number = number
   end
 
+  def self.for(number)
+    case number
+    when 0
+      BottleNumber0
+    when 1
+      BottleNumber1
+    else
+      BottleNumber
+    end.new(number)
+  end
+
   def to_s
     "#{quantity} #{container}"
   end
 
   def successor
-    number - 1
+    BottleNumber.for(number - 1)
   end
 
   def action
@@ -62,7 +73,7 @@ end
 
 class BottleNumber0 < BottleNumber
   def successor
-      99
+    BottleNumber.for(99)
   end
 
   def quantity
