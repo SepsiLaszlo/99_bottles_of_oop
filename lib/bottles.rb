49e6b6be1561ba5a73305d15b1619c2b58fc6d15
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2020-07-02 21:56:32 +0200

    Prying open the Factory

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,17 +7,6 @@ class Bottles
     starting.downto(ending).map { |i| verse(i) }.join("\n")
   end
 
-  def bottle_number_for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    else
-      BottleNumber
-    end.new(number)
-  end
-
   def verse(number)
     bottle_number = BottleNumber.for(number)
     next_bottle_number = bottle_number.successor
@@ -36,12 +25,9 @@ class BottleNumber
   end
 
   def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    else
+    begin
+      const_get("BottleNumber#{number}")
+    rescue NameError
       BottleNumber
     end.new(number)
   end
